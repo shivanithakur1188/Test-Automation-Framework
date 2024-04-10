257dabdeb9f82eb2d7581b047d81d48da67ae3a6
ziya.azam@globallogic.com <ziya.azam@globallogic.com>
2024-04-10 16:07:22 +0530

    Added serenity.conf file to support environment level test runs for UI

--- src/test/java/com/taf/api/utilities/Utility.java
+++ src/test/java/com/taf/api/utilities/Utility.java
@@ -21,6 +21,7 @@ import io.restassured.response.Response;
 import io.restassured.specification.RequestSpecification;
 import net.serenitybdd.rest.SerenityRest;
 import com.taf.base.Base;
+import com.taf.base.MyLogger;
 
 /**
  * The Class Utility.
@@ -150,6 +151,7 @@ public class Utility {
 			}
 
 		}
+		MyLogger.info("Request Body:\n"+jsonBody);
 		return jsonBody;
 	}
 
