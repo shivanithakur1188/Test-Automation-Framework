45bfa632d9530e917d22a92fd03746952d495f22
mdrehan.akhter <mdrehan.akhter@globallogic.com>
2024-04-20 12:24:56 +0530

    Added API Scenarios for GCH

--- src/test/java/com/taf/api/utilities/JsonPath.java
+++ src/test/java/com/taf/api/utilities/JsonPath.java
@@ -2,6 +2,9 @@ package com.taf.api.utilities;
 
 public class JsonPath {
     public static final String JSON_PATH_ID = "id";
+
+    /* --------Create New Article------*/
+    public static final String JSON_PATH_ARTICLE_ID = "data.id";
     
 //    public static void main(String[] args) {}
 }
