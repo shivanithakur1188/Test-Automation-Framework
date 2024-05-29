4e817f9387d127a94ec0bfce21b04d068357793f
ziya.azam@globallogic.com <ziya.azam@globallogic.com>
2024-05-29 10:38:23 +0530

    Updated Driver logic

--- src/test/java/com/taf/runner/TestRunner.java
+++ src/test/java/com/taf/runner/TestRunner.java
@@ -7,5 +7,5 @@ import org.junit.runner.RunWith;
 
 @RunWith(CucumberWithSerenity.class)
 @CucumberOptions(features="src/test/resources/features/", glue="com.taf",tags = "@api")
-public class Runner {}
+public class TestRunner {}
  
